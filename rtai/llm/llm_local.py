51131b4d0ce5c3219a660fb0751131604e95810b
Neil Yeung <nyeung@MacBook-Pro-7.attlocal.net>
2024-01-08 00:39:12 -0800

    added thought and importance functions; got basic embeddings to work

--- rtai/llm/llm_local.py
+++ rtai/llm/llm_local.py
@@ -5,19 +5,23 @@ from guidance import models, gen
 import guidance
 from rtai.utils.config import Config
 
-# global llm model
-
-model = None
+model = None # global model variable
 
 def load_model(cfg: Config):
     global model
-    model = models.LlamaCpp(cfg.get_value("local_model_path", ""), n_gpu_layers=-1, n_ctx=20000)
-    model.echo = False
+    model = models.LlamaCpp(cfg.get_value("local_model_path", ""), n_gpu_layers=-1, n_ctx=2048)
+    # model.echo = False
 
 @guidance
 def create_daily_tasks(lm, persona, num_tasks=3):
     for i in range(num_tasks):
-        lm += f'''Task {i+1} that {persona} does in a day: "{gen(stop='"', name="tasks", temperature=1.0, list_append=True, max_tokens=100)}"\n'''
+        lm += f'''Briefly describe a task {i+1} that {persona} does in a day in 10 or less words: "{gen(stop=".", name="tasks", temperature=1.0, list_append=True)}"\n'''
+    return lm
+
+@guidance 
+def create_thought(lm, persona, situation, num_thoughts=3):
+    for i in range(num_thoughts):
+        lm += f'''Generate a short thought {i+1} that {persona} might have in the following situation: {situation}. "Here is thought {i+1}: {gen(stop='.', name="thoughts", temperature=1.0, list_append=True)}"\n'''
     return lm
 
 @guidance
@@ -29,13 +33,23 @@ def estimate_duration(lm, persona, tasks):
 
 @guidance
 def estimate_start_times(lm, persona, tasks):
-    lm +=  f"Generate a start time for when {persona} will start each task: \n"
+    lm += f"Generate a start time for when {persona} will start each task: \n"
     for i in range(len(tasks)):
         lm += f'''Task {i+1} will start at {gen(stop='"', regex="[0-9]:[0-9][0-9]", name="start_time", temperature=0.7, max_tokens=10, list_append=True)} hours\n'''
     return lm
 
 @guidance
+def estimate_importance(lm, persona, observations):
+    lm += f"On the scale of 1 to 10, where 1 is purely mundane and 10 is extremely important (e.g., a break up, college acceptance), rate the importance of the following observation to {persona}: \n" 
+    for i in range(len(observations)):
+        lm += f'''Observation: {observations[i]}. The importance of this observation is: {gen(stop='"', regex="[0-9]", name="importance", temperature=0.7, max_tokens=10, list_append=True)} \n'''
+    return lm
+
+
+
+@guidance
 def create_dialogue(persona1, persona2, location):
+    # TODO: debug this diaglogue for robustness
     global model
     dialogue_prompt = f"""
     Generate a short dialogue between {persona1.name} and {persona2.name} in {location}
@@ -57,14 +71,24 @@ def generate_daily_plan(persona):
     # generate the tasks
     out1 = model + create_daily_tasks(persona)
     tasks = out1['tasks']
-    print(tasks)
+    # print(tasks)
     # estimate the duration
     out2 = model + estimate_duration(persona, tasks)
     duration = out2["duration"]
-    print(duration)
+    # print(duration)
     # estimate the start times
     out3 = model + estimate_start_times(persona, tasks)
     start_time = out3["start_time"]
-    print(start_time)
+    # print(start_time)
     # return a list of triples
     return list(zip(tasks, duration, start_time))
+
+def generate_thought(persona, situation):
+    global model
+    out = model + create_thought(persona, situation)
+    thoughts = out["thoughts"]
+    print(thoughts)
+
+    out2 = model + estimate_importance(persona, thoughts)
+    importance = out2["importance"]
+    return list(zip(thoughts, importance))
