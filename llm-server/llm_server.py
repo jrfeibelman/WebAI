1a45c7ab5ac6352f7956d4466bdcb88eb2abce84
Genuinely <neil.y.yeung@gmail.com>
2024-01-18 16:31:29 -0500

    scaffolding for the server so it can dynamically load prompts; aiming to create easy way to run demo material

--- llm-server/llm_server.py
+++ llm-server/llm_server.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI
-from guidance import models, gen
-import guidance
-from prompts import create_daily_tasks
+from guidance import models
+from prompts import generate_dialogue_about_topic
 from pydantic import BaseModel
+from typing import Dict, Any
 
-mistral = models.LlamaCpp("/Users/nyeung/Projects/llama.cpp/models/mistral-7b-instruct-v0.2.Q4_K_M.gguf", n_context=2048, caching=False)
+mistral = models.LlamaCpp("/Users/nyeung/Projects/llama.cpp/models/mistral-7b-instruct-v0.2.Q4_K_M.gguf", n_context=2048, caching=True)
 mistral.echo = False
-
 
 class Request(BaseModel):
     prompt: str
+    task: str
+    args: Dict[str, Any]
 
 app = FastAPI()
 
 @app.post("/")
 def generate(request: Request):
-    lm = mistral + request.prompt
-    out = lm + create_daily_tasks()
-    tasks = out["tasks"]
-    return tasks[0]
+    lm = mistral + request.prompt # story telling prompt
+    dialogue_topic, location = request.args["dialogue_topic"], request.args["location"]
+    out = lm + generate_dialogue_about_topic(dialogue_topic, location)
+    output = out["dialogue"]
+    return output
