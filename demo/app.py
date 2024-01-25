86d541c9a64477e14f7cdcb882026347c0fd0238
Genuinely <neil.y.yeung@gmail.com>
2024-01-24 21:11:11 -0500

    demo

--- demo/app.py
+++ demo/app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import pandas as pd
 import os
+import json
+import html
+
+def read_json(f_path):
+    return json.loads(open(f_path).read())
+
+def render_html(raw_string):
+    escaped_string = html.escape(raw_string)
+    return escaped_string
 
 def read_and_display_csvs(folder_path):
     # Get a list of all CSV files in the folder
@@ -18,6 +27,18 @@ def read_and_display_csvs(folder_path):
 
 # to render the chat as a chat bubbles
 def render_chat(chat_df: pd.DataFrame):
+    # retrieve context before chat
+    st.image('images/lawyer.png', width=100)
+    context_raw = "There is a potential alibi for Emily during the time of the poisoning, involving a witness not yet presented in court.Mark possesses an anonymous letter received by the Thorntons, hinting at threats from an unknown source.Richard was involved in a secret business deal that might have contributed to his strained relationships."
+    context_html = render_html(context_raw)
+    st.markdown(
+        f'<div style="background-color: #add8e6; padding: 10px; margin: 10px; border-radius: 10px; display: inline-block; margin-left: 60px;">'
+        f'<b>Agent:</b> Mark (Retrieveing 🧠...)<br>'
+        f'<b>Retrieved Context:</b> {context_html}<br>'
+        '</div>', unsafe_allow_html=True
+    )
+
+    st.divider()
     # Dictionary mapping speakers to image paths
     speaker_images = {
         'Mark': 'images/lawyer.png',  # TODO: change this to be configurable
@@ -38,21 +59,18 @@ def render_chat(chat_df: pd.DataFrame):
                 '</div>', unsafe_allow_html=True
             )
 
-    st.image('images/lawyer.png', width=100)
-    st.markdown(
-        f'<div style="background-color: #add8e6; padding: 10px; margin: 10px; border-radius: 10px; display: inline-block; margin-left: 60px;">'
-        f'<b>Agent:</b> Mark (Retrieveing 🧠...)<br>'
-        f'<b>Retrieved:</b> There is a potential alibi for Emily during the time of the poisoning, involving a witness not yet presented in court.Mark possesses an anonymous letter received by the Thorntons, hinting at threats from an unknown source.Richard was involved in a secret business deal that might have contributed to his strained relationships.<br>'
-        '</div>', unsafe_allow_html=True
-    )
+ 
     # place for the reflection
     st.divider()
 
     st.image('images/lawyer.png', width=50)
+    raw_string = "The anonymous letter could provide significant insight into Richard's"
+    raw_string = read_json('mark_reflection1.json')
+    escaped_string = ''.join([render_html(a) for a in raw_string])
     st.markdown(
         f'<div style="background-color: #add8e6; padding: 10px; margin: 10px; border-radius: 10px; display: inline-block; margin-left: 60px;">'
         f'<b>Agent:</b> Mark (Reflecting 🤔...)<br>'
-        f'<b>Reflection: </b> <em> Emily admitted to leaving the room after the heated argument with Richard, providing a potential gap in time for the poisoning to have occurred.There is a potential alibi for Emily during the time of the poisoning, involving a witness not yet presented in court. Emily\'s financial struggles and Richard\'s infidelity may have been motives for the poisoning, but they do not definitively prove Emily\'s guilt. These details add complexity to the case and make it more intriguing for Mark to investigate further.</em>'
+        f'<b>Reflection: </b> <em> {escaped_string} </em>'
         '</div>', unsafe_allow_html=True
     )
 
@@ -72,32 +90,48 @@ def main():
     st.title("Mythos")
     st.header("Shadows of Deceit")
     folder_path = "test_dialogues/"
+    # memory_before_path = "memory_before.json"
+    # memory_before = json.loads(open(memory_before_path).read())
+    mark_facts = [
+    "Richard had business rivals who might have had motives to harm him.",
+    "There is a potential alibi for Emily during the time of the poisoning, involving a witness not yet presented in court.",
+    "Inconsistencies exist in the initial police investigation, including gaps in the timeline and overlooked potential suspects.",
+    "Richard was involved in a secret business deal that might have contributed to his strained relationships.",
+    "Mark possesses an anonymous letter received by the Thorntons, hinting at threats from an unknown source."
+    ]
+    # mark_memory_after = json.loads(open('mark_facts_after.json').read())
+    mark_memory_after = read_json('mark_facts_after.json')
 
-    with st.expander("Memory Before 🧠"):
-        st.markdown(":green[NEW MEMORY: There is a potential alibi for Emily during the time of the poisoning, involving a witness not yet presented in court.]")
+    ###### --------------MEMORY BEFORE--------------######
+    with st.expander("Mark's Memory Before 🧠"):
+        for i, fact in enumerate(mark_facts):
+            st.markdown(f"{i}: {fact}")
+        # st.markdown(":green[NEW MEMORY: There is a potential alibi for Emily during the time of the poisoning, involving a witness not yet presented in court.]")
     
-    
+    #### --------------DIALOGUES--------------######
     visualize_chat_dataframes(folder_path)
+    
+    #### --------------MEMORY AFTER--------------######
     with st.expander("Memory After 🧠"):
-        st.json({
-            "Mark": {
-                "name": "Mark",
-                "memory": [
-                    "There is a potential alibi for Emily during the time of the poisoning, involving a witness not yet presented in court.",
-                    "Mark possesses an anonymous letter received by the Thorntons, hinting at threats from an unknown source.",
-                    "Richard was involved in a secret business deal that might have contributed to his strained relationships."
-                ]
-            }
-        })
+        for i, fact in enumerate(mark_memory_after):
+            if i > 4:
+                fact_string = f":green[NEW MEMORY: {fact}]"
+            else:
+                fact_string = f"{i}: {fact}"
+            st.markdown(fact_string)
+    
+    #### --------------RESULTING STORY--------------######
     # TODO: handle the escape chars
     with st.expander("Story 📖"):
-        st.write(f'''As the sun dipped below the horizon, Mark Turner's mind remained wide awake, illuminated by the eerie glow of the moonlight. Sifting through intricate threads of evidence, he embarked on a journey for the truth. Raw intelligence danced in his eyes, as he sat before the fireplace engulfed in the dimly-lit room.
-    "Just a whisper of doubt lingered in the air, arising from Sarah Reynolds' hesitant testimony. ""I saw a man in a red tie leave the room,"" she had confided, but Mark could sense her unease. He wondered if she was holding backी"\n"
+        raw_string2 = '''As the sun dipped below the horizon, Mark Turner's mind remained wide awake, illuminated by the eerie glow of the moonlight. Sifting through intricate threads of evidence, he embarked on a journey for the truth. Raw intelligence danced in his eyes, as he sat before the fireplace engulfed in the dimly-lit room.
+    "Just a whisper of doubt lingered in the air, arising from Sarah Reynolds' hesitant testimony. ""I saw a man in a red tie leave the room,"" she had confided, but Mark could sense her unease. He wondered if she was holding back\n"
     "Mark's gaze automatically swung to his reflection in the polished glass facade, zeroing in on the deep creases that tugged at the corners of his eyes. ""We'll get to the bottom of this, Sarah,"" he assured her earlier that day, but uncertainty lurked in the shadows."\n
     "Across the bustling courtroom, Starchy-haired Emily Thorton patiently awaited her fate, her pastel gown a stark contrast to the smoky room. Her eyes, dancing with a mixture of nervous anticipation and stubborn resolve, told a compelling story of a woman falsely accused."\n
     "Her tale of a marital strife gone awry now inextricably linked to Sarah's fragmented account. Three dots connected: one unearthed secret, two shrouded truths, and a wealth of deception."\n
     "Mark knew Emily couldn't possibly have orchestrated Richard's untimely demise, but who orchestrated the web of lies? The burning question gnawed at him, desperate for an answer."\n
-    "As Mark continued to sift through the intricate threads of the case, he couldn't shake the feeling that something was amiss. He needed another piece of the puzzle, a revelation that would clear the murky waters and shed light on the mystery that the moonlit night could not quite reveal.''')
+    "As Mark continued to sift through the intricate threads of the case, he couldn't shake the feeling that something was amiss. He needed another piece of the puzzle, a revelation that would clear the murky waters and shed light on the mystery that the moonlit night could not quite reveal.'''
+        raw_string2 = render_html(raw_string2)
+        st.markdown(unsafe_allow_html=True, body=raw_string2)
 
 if __name__ == "__main__":
     main()
